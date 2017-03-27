38a195829856cd7557278bf93d04929aea9ba5a6
Dido Maulana <dmaulana96@hotmail.com>
2017-03-27 15:10:03 -0230

    Database connection added

--- chooseVehicle.php
+++ chooseVehicle.php
@@ -4,6 +4,15 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+
+include 'sqlSetup.php';
+//session_start();
+
+
+
+?>
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -27,26 +36,50 @@ and open the template in the editor.
           Choose Vehicle
             
         </div>
-        
+            <?php 
+            $carQuery = mysqli_query($link,'SELECT model, type,year, img,ppd FROM cars WHERE branchID = "1"'); ?>
         
               <div id="summary">          
                   <a> Rental Summary </a>
-            
+                  <li> </li>
+                  
             </div>
         
         <div id="cars" >
             
-        <div id="menuBar" >
+            <div id="menuBar" >
             <a href="#All">All</a>
             <a href="#Sedan">Sedan</a>
             <a href="#4x4">4X4</a>
             <a href="#Coupe">Coupe</a>
             <a href="#Hatchback">Hatchback</a>
-        </div>
+            </div>
          
+         
+            
+            <script src="javascript/clickableRow.js"></script>
+            <table>
+               <?php foreach ($carQuery as $cars): ?>
+
+                <tr class = 'clickableRow'> 
+                    <td> <a href="carModel"> <?php echo $cars['model']; ?>  </a>
+                        <br> <?php echo $cars['year']; ?> </br></td>
+                    <td> <a>   <?php echo $cars['type'];?> </a> </td>
+                    
+                    
+                    <td>    
+                        <img src= "<?php echo $cars['img']; ?>" height="150" width ="200" />
+                         </td>
+                </tr>
+                     <?php endforeach; ?>
+               
+            </table>
+                       
+        </div>
+       
       
             
-        </div>
+      
           
         
         <?php
--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+  <?php
+          $server = "mysql.cs.mun.ca";
+          $name = "cs3715w17_dfm672";
+          $password = "qQTgScX3";
+          $database = "cs3715w17_dfm672";
+            session_start();
+//            $server =  "localhost";
+//            $name = "root";
+//            $password = "";
+//            $database = "dvg";
+            
+            $link = new mysqli($server, $name, $password, $database);
+            
+            if ($link -> connect_error) 
+                {
+                die('Something went wrong while connecting to MSSQL');
+                }
+            
+  
+   
+            ?>
+        
 <html>
 	<head>
 		<title>DVG Rental</title>
