24b96cff9eb79783293b7892240506510c44abcc
Dido Maulana <dmaulana96@hotmail.com>
2017-03-30 19:14:42 -0230

    Uploaded my log

--- Checkout-proj.php
+++ Checkout-proj.php
@@ -177,15 +177,14 @@ Card Number:<br>
           
 
 <?php
-
-
+session_start();
 include 'sqlSetup.php';
 
     $startDate1=$_POST["var1"];
     $endDate1=$_POST["var2"];
    
-     
-$type_of_car=$_SESSION["var3"];
+     $car_price=0.0;
+		$type_of_car=$_POST["var3"];
     
 if ($type_of_car == "Sedan"){
 	$car_price=20;
@@ -195,7 +194,6 @@ if ($type_of_car == "Sedan"){
 	}
 	else if($type_of_car=="SUV"){
 		$car_price=25;
-		print $car_price;
 	}
 	
 
